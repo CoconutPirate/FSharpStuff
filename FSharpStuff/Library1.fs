a1e672ef110257697eea99a45356b02ca9c9fc7f
Lukasz Skotarek <Lukasz.Skotarek@powel.com>
2015-08-04 10:30:58 +0200

    FSunit working
    2.2 and 2.3 done

--- FSharpStuff/Library1.fs
+++ FSharpStuff/Library1.fs
@@ -4,3 +4,16 @@ module TwoPointOne =
     type Divisor () =
         member this.checkForDivision x =
             (x % 2 = 0 || x % 3 = 0) && x % 5 <> 0
+
+module TwoPointTwo =
+    type Stringifator () =
+        member this.power s n =
+            match n with
+            | 1 -> sprintf "%s" s
+            | _ -> (sprintf "%s * " s) + (this.power s (n-1))
+            
+
+module TwoPointThree =
+    type StringCharChecker () =
+        member this.isIthChar (s:string) (i:int) (c:char) =
+            s.Chars i = c
--- FSharpStuff/Tests.fs
+++ FSharpStuff/Tests.fs
@@ -2,29 +2,58 @@
 
 open NUnit.Framework
 open FsUnit
-open FSharpStuff.TwoPointOne
+
+    [<TestFixture>]
+    type ``Excercise 2point1`` () =
+        let divisor = new FSharpStuff.TwoPointOne.Divisor()
+
+        [<Test>] member test.
+            ``24 should give true as divisible by 2 and 3`` () =
+                divisor.checkForDivision 24 
+                |> should be True
+
+        [<Test>] member test.
+            ``27 should give true as divisible by 3`` () =
+                divisor.checkForDivision 27 
+                |> should be True
+
+        [<Test>] member test.
+            ``30 should give false as divisable by 5`` () =
+                divisor.checkForDivision 30 
+                |> should be False
+
+        [<Test>] member test.
+            ``29 should give false as not divisible`` () =
+                divisor.checkForDivision 29 
+                |> should be False
 
 
     [<TestFixture>]
-    type ``Excercise 2point1`` ()=
-        let divisor = new Divisor()
+    type ``Excercise 2point2`` () =
+        let stringifator = new FSharpStuff.TwoPointTwo.Stringifator()
 
         [<Test>] member test.
-         ``24 should give true as divisible by 2 and 3`` ()=
-               divisor.checkForDivision 24 
-               |> should be True
+            ``pow 4 1 should return 4`` () =
+                stringifator.power "4" 1
+                |> should equal "4"
 
         [<Test>] member test.
-         ``27 should give true as divisible by 3`` ()=
-               divisor.checkForDivision 27 
-               |> should equal True
+            ``pow 5 3 should return 5 * 5 * 5 `` () =
+                stringifator.power "5" 3
+                |> should equal "5 * 5 * 5"
+
+
+
+    [<TestFixture>]
+    type ``Excercise 2point3`` () =
+        let checker = new FSharpStuff.TwoPointThree.StringCharChecker()
 
         [<Test>] member test.
-         ``30 should give false as divisable by 5`` ()=
-               divisor.checkForDivision 30 
-               |> should equal False
+            ``should be true when char is ith char of the string`` () =
+                checker.isIthChar "abc" 1 'b'
+                |> should be True
 
         [<Test>] member test.
-         ``29 should give false as not divisible`` ()=
-               divisor.checkForDivision 29 
-               |> should equal False
+            ``should be false when char is not ith char of the string`` () =
+                checker.isIthChar "abc" 1 'z'
+                |> should be False
