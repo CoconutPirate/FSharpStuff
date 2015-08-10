75c9b8760545cce0b838a71bc2d2c007d360bbef
dreat <lukskotarek@gmail.com>
2015-08-10 20:57:38 +0200

    Chapter 3 started, half of the 3.1 done

---
+++ FSharpStuff/ChapterThree.fs
@@ -0,0 +1,16 @@
+﻿namespace FSharpStuff
+
+module ThreePointOne =
+    let checkTimeWithTriples (firstHours, firstMinutes, firstF) (secondHours, secondMinutes, secondF) =
+        let firstHour = if firstF.Equals "PM" then firstHours + 12 else firstHours
+        let secondHour = if secondF.Equals "PM" then secondHours + 12 else secondHours
+        match firstHour with
+        | x when x < secondHour -> false
+        | x when x > secondHour -> true
+        | x when x = secondHour -> match firstMinutes with
+                                    | x when x < secondMinutes -> false
+                                    | x when x > secondMinutes -> true
+                                    | _ -> false
+        | _ -> false
+    //todo: records
+
---
+++ FSharpStuff/ChapterThreeTests.fs
@@ -0,0 +1,39 @@
+﻿namespace FSharpStuff.Tests
+
+open NUnit.Framework
+open FsUnit
+open FSharpStuff.ThreePointOne
+
+
+ [<TestFixture>]
+    type ``Excercise 3point1`` () =
+
+        [<Test>] member test.
+            `` 11:30 AM should be treated as later than 10:30 AM `` () =
+                checkTimeWithTriples (11,30,"AM") (10, 30, "AM")
+                |> should be True
+
+        [<Test>] member test.
+            ``11:30 AM should be treated as later than 11:25 AM `` () =
+                checkTimeWithTriples (11,30, "AM") (11,25, "AM") 
+                |> should be True
+
+        [<Test>] member test.
+            `` 10:30 AM should be treated as earlier than 11:30 AM `` () =
+                checkTimeWithTriples (10,30,"AM") (11, 30, "AM")
+                |> should be False
+
+        [<Test>] member test.
+            ``11:25 AM should be treated as later than 11:30 AM `` () =
+                checkTimeWithTriples (11,25, "AM") (11,30, "AM") 
+                |> should be False
+
+        [<Test>] member test.
+            ``11:30 AM should be treated as not later than 11:30AM `` () =
+                checkTimeWithTriples (11,30,"AM") (11,30,"AM")
+                |> should be False
+
+        [<Test>] member test.
+            ``10:30 PM should be treated as later than 11:30 AM `` () =
+                checkTimeWithTriples (10,30,"PM") (11,30,"AM") 
+                |> should be True
