433a1c021cc01c887f32038ec8998a7884cf9f5e
Lukasz Skotarek <Lukasz.Skotarek@powel.com>
2015-09-21 16:36:25 +0200

    done rest of 3.1, whole 3.2 and 3.3

--- FSharpStuff/ChapterThree.fs
+++ FSharpStuff/ChapterThree.fs
@@ -1,16 +1,62 @@
 ﻿namespace FSharpStuff
 
 module ThreePointOne =
-    let checkTimeWithTriples (firstHours, firstMinutes, firstF) (secondHours, secondMinutes, secondF) =
-        let firstHour = if firstF.Equals "PM" then firstHours + 12 else firstHours
-        let secondHour = if secondF.Equals "PM" then secondHours + 12 else secondHours
-        match firstHour with
-        | x when x < secondHour -> false
-        | x when x > secondHour -> true
-        | x when x = secondHour -> match firstMinutes with
-                                    | x when x < secondMinutes -> false
-                                    | x when x > secondMinutes -> true
-                                    | _ -> false
-        | _ -> false
-    //todo: records
+    type RecordDate = {hour: int; minute: int; f: string}
 
+    let checkForF hour f =
+        if f.Equals "PM" then hour + 12 else hour
+
+    let (.>.) date1 date2 =
+        let firstHour = checkForF date1.hour date1.f
+        let secondHour = checkForF date2.hour date2.f
+        (firstHour, date1.minute) > (secondHour, date2.minute)
+
+    let (>.) (firstHours, firstMinutes, firstF) (secondHours, secondMinutes, secondF) =
+        let firstHour = checkForF firstHours firstF
+        let secondHour = checkForF secondHours secondF
+        (firstHour, firstMinutes) > (secondHour, secondMinutes)
+ 
+module ThreePointTwo =
+    
+    let rec matchShillings (pound1, shilling1, pence1) (pound2, shilling2, pence2) =
+        match shilling1+shilling2 with
+        | x when x >= 20 -> matchShillings (pound1+1, shilling1-20, pence1) (pound2, shilling2, pence2)
+        | _ -> (pound1+pound2, shilling1+shilling2, pence1+pence2)
+
+    let rec (+.) (pound1, shilling1, pence1) (pound2, shilling2, pence2) =
+        match pence1 + pence2 with
+        | x when x >= 12 -> (pound1, shilling1+1, pence1-12) +. (pound2, shilling2, pence2)
+        | _ -> matchShillings (pound1, shilling1, pence1) (pound2, shilling2, pence2)
+        
+    let pencify (pound, shilling, pence) =
+        pound * 20 * 12 + shilling * 12 + pence
+
+    let (-.) (pound1, shilling1, pence1) (pound2, shilling2, pence2) =
+        let pences1 = pencify (pound1, shilling1, pence1)
+        let pences2 = pencify (pound2, shilling2, pence2)
+        
+        let pences = pences1 - pences2
+        (0,0,pences)+.(0,0,0)        
+
+
+module ThreePointThree =
+    type Complex = {real: double; imaginary: double}
+
+    let (.+.) a b  =
+        {real = a.real + b.real; imaginary = a.imaginary + b.imaginary}
+
+    let (.*.) a b =
+        {real = a.real * b.real - a.imaginary * b.imaginary; imaginary = a.imaginary * b.real + a.real * b.imaginary}
+
+    let inverseAdd a =
+        {real = -a.real; imaginary = -a.imaginary}
+
+    let inverseMulti a =
+        let denominator = a.real ** 2. + a.imaginary ** 2.
+        {real = a.real / denominator; imaginary = -a.imaginary / denominator}
+
+    let (.-.) a b =
+        a .+. (inverseAdd b)
+
+    let (./.) a b =
+        a .*. (inverseMulti b)
--- FSharpStuff/ChapterThreeTests.fs
+++ FSharpStuff/ChapterThreeTests.fs
@@ -3,37 +3,140 @@
 open NUnit.Framework
 open FsUnit
 open FSharpStuff.ThreePointOne
+open FSharpStuff.ThreePointTwo
+open FSharpStuff.ThreePointThree
 
 
  [<TestFixture>]
     type ``Excercise 3point1`` () =
 
         [<Test>] member test.
-            `` 11:30 AM should be treated as later than 10:30 AM `` () =
-                checkTimeWithTriples (11,30,"AM") (10, 30, "AM")
+            ``tripe: 11:30 AM should be treated as later than 10:30 AM `` () =
+                (11,30,"AM") >. (10, 30, "AM")
                 |> should be True
 
         [<Test>] member test.
-            ``11:30 AM should be treated as later than 11:25 AM `` () =
-                checkTimeWithTriples (11,30, "AM") (11,25, "AM") 
+            ``triple: 11:30 AM should be treated as later than 11:25 AM `` () =
+                (11,30, "AM") >. (11,25, "AM") 
                 |> should be True
 
         [<Test>] member test.
-            `` 10:30 AM should be treated as earlier than 11:30 AM `` () =
-                checkTimeWithTriples (10,30,"AM") (11, 30, "AM")
+            ``triple: 10:30 AM should be treated as earlier than 11:30 AM `` () =
+                (10,30,"AM") >. (11, 30, "AM")
                 |> should be False
 
         [<Test>] member test.
-            ``11:25 AM should be treated as later than 11:30 AM `` () =
-                checkTimeWithTriples (11,25, "AM") (11,30, "AM") 
+            ``triple: 11:25 AM should be treated as later than 11:30 AM `` () =
+                (11,25, "AM") >. (11,30, "AM") 
                 |> should be False
 
         [<Test>] member test.
-            ``11:30 AM should be treated as not later than 11:30AM `` () =
-                checkTimeWithTriples (11,30,"AM") (11,30,"AM")
+            ``triple: 11:30 AM should be treated as not later than 11:30AM `` () =
+                (11,30,"AM") >. (11,30,"AM")
                 |> should be False
 
         [<Test>] member test.
-            ``10:30 PM should be treated as later than 11:30 AM `` () =
-                checkTimeWithTriples (10,30,"PM") (11,30,"AM") 
+            ``triple: 10:30 PM should be treated as later than 11:30 AM `` () =
+                (10,30,"PM") >. (11,30,"AM") 
                 |> should be True
+
+        [<Test>] member test.
+            ``record: 11:30 AM should be treated as later than 10:30 AM `` () =
+                {hour = 11 ; minute = 30 ; f = "AM"} .>. { hour = 10 ; minute = 30; f = "AM"}
+                |> should be True
+
+        [<Test>] member test.
+            ``record: 11:30 AM should be treated as later than 11:25 AM `` () =
+                 {hour = 11 ; minute = 30 ; f = "AM"} .>.  {hour = 11 ; minute = 25 ; f = "AM"}
+                |> should be True
+
+        [<Test>] member test.
+            ``record: 10:30 AM should be treated as earlier than 11:30 AM `` () =
+                { hour = 10 ; minute = 30; f = "AM"} .>.  {hour = 11 ; minute = 30 ; f = "AM"}
+                |> should be False
+
+        [<Test>] member test.
+            ``record: 11:25 AM should be treated as later than 11:30 AM `` () =
+                {hour = 11 ; minute = 25 ; f = "AM"} .>. {hour = 11 ; minute = 30 ; f = "AM"}
+                |> should be False
+
+        [<Test>] member test.
+            ``record: 11:30 AM should be treated as not later than 11:30AM `` () =
+                {hour = 11 ; minute = 30 ; f = "AM"} .>. {hour = 11 ; minute = 30 ; f = "AM"}
+                |> should be False
+
+        [<Test>] member test.
+            ``record: 10:30 PM should be treated as later than 11:30 AM `` () =
+                { hour = 10 ; minute = 30; f = "PM"} .>. {hour = 11 ; minute = 30 ; f = "AM"}
+                |> should be True
+
+    type ``Excercise 3point2`` () =
+        [<Test>] member test.
+            `` 1 pound + 1 shilling + 1 pence = 1f1s1p `` () =
+                (1,0,0)+.(0,1,0)+.(0,0,1)
+                |> should equal (1,1,1)
+
+        [<Test>] member test.
+            `` 6 + 6 pences is 1 shilling `` () =
+                (0,0,6)+.(0,0,6)
+                |> should equal (0,1,0)
+
+        [<Test>] member test.
+            `` 10 + 10 shillings is 1 pound `` () =
+                (0,10,0)+.(0,10,0)
+                |> should equal (1,0,0)
+
+        [<Test>] member test.
+            `` (1,9,8) + (2, 11, 8) = (4,1,4) `` () =
+                (1,9,8)+.(2,11,8)
+                |> should equal (4,1,4)
+
+        [<Test>] member test.
+            `` (3,3,3) - (2,2,2) = (1,1,1) `` () =
+                (3,3,3) -. (2,2,2)
+                |> should equal (1,1,1)
+
+        [<Test>] member test.
+            `` 1 pound - 5 shillings = 15 shilling `` () =
+                (1,0,0) -. (0,5,0)
+                |> should equal (0,15,0)
+
+        [<Test>] member test.
+            `` 1 shilling - 5 pences = 7 pences `` () =
+                (0,1,0) -. (0,0,5)
+                |> should equal (0,0,7)
+
+        [<Test>] member test.
+            `` (4,1,4) - (2, 11, 8) = (1,9,8) `` () =
+                (4,1,4) -. (2,11,8)
+                |> should equal (1,9,8)
+    type `` Excercise 3point3`` () =
+        [<Test>] member test.
+            `` 3+5i + 1+4i = 4+9i `` () =
+                {real = 3.; imaginary = 5.} .+. {real = 1.; imaginary = 4.}
+                |> should equal {real = 4.; imaginary = 9.}
+
+        [<Test>] member test.
+            `` 3+5i * 1+4i = 3-20 + 5+12i `` () =
+                {real = 3.; imaginary = 5.} .*. {real = 1.; imaginary = 4.}
+                |> should equal {real = -17.; imaginary = 17.}
+
+        [<Test>] member test.
+            `` 3-5i inversed with regard to addition = -3+5i `` () =
+                inverseAdd {real = 3.; imaginary = -5.}
+                |> should equal {real = -3.; imaginary = 5.}
+
+        [<Test>] member test.
+            `` 2+4i inversed with regard to multiplication should equal 0.1-0.2i `` () =
+                inverseMulti {real = 2.; imaginary = 4.}
+                |> should equal {real = 0.1; imaginary = -0.2}
+
+        [<Test>] member test.
+            `` 4 + 9i - 1+4i = 3+5i `` () =
+                {real = 4.; imaginary = 9.} .-. {real = 1.; imaginary = 4.}
+                |> should equal {real = 3.; imaginary = 5.}
+
+        [<Test>] member test.
+            `` -17+17i / 1+4i = 3+5i `` () =
+                {real = -17.; imaginary = 17.} ./. {real = 1.; imaginary = 4.}
+                |> should equal {real = 3.; imaginary = 5.}
